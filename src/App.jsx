f143965a358165a97b5707c4ee1d8a6d6983a136
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-25 23:01:16 -0300

    zx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 import pokemonData from './pokemonData.json';
 
+function Header() {
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img
+          src="public\Imagenes Dulces\DULCE.jpg"
+          className="logo"
+          alt="Vite logo"
+        />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img
+          src="public\Imagenes Dulces\DULCE.jpg"
+          className="logo react"
+          alt="React logo"
+        />
+      </a>
+      <h1>Cotizador de Pokémon</h1>
+    </div>
+  );
+}
+
+function CotizadorForm({ cotizarPokemon, pokemonData }) {
+  return (
+    <div id="pokemon-form">
+      <label htmlFor="pokemon-name">Nombre del Pokémon:</label>
+      <select id="pokemon-name">
+        {pokemonData.pokemon.map((pokemon, index) => (
+          <option key={index} value={index}>
+            {pokemon.name}
+          </option>
+        ))}
+      </select>
+
+      <label htmlFor="pokemon-height">Altura (en cm):</label>
+      <input type="number" id="pokemon-height" min="23" max="140" />
+
+      <label htmlFor="pokemon-dna">Tipo de ADN (extrato):</label>
+      <select id="pokemon-dna">
+        {pokemonData.pokemon.map((pokemon, index) => (
+          <option key={index} value={index}>
+            {pokemon.dna}
+          </option>
+        ))}
+      </select>
+
+      <button
+        type="button"
+        onClick={() => cotizarPokemon(pokemonData.pokemon[document.getElementById('pokemon-name').value])}
+      >
+        Calcular
+      </button>
+    </div>
+  );
+}
+
+function HistorialCotizaciones({ cotizaciones }) {
+  return (
+    <div id="historial">
+      <h2>Historial de Cotizaciones</h2>
+      <ul id="cotizaciones">
+        {cotizaciones.map((cotizacion, index) => (
+          <li key={index}>{cotizacion}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
   const [showCotizador, setShowCotizador] = useState(true);
@@ -32,48 +98,14 @@ function App() {
 
   return (
     <div className="App">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="https://scontent.fnqn4-1.fna.fbcdn.net/v/t39.30808-6/391626291_7067940753297285_6954890977725599637_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHJ2qLTodrcoNTlc1TIDsCjf99v5hG6fqx_32_mEbp-rLm-XJ6xAz5PCdh40uC9wIrvSm_l331_zryGks6CNy_e&_nc_ohc=d1H5-ZdsUxkAX_J4b95&_nc_ht=scontent.fnqn4-1.fna&oh=00_AfCbot7RlyQnc6fmUHHfWDnlHiW-y5EQwUu3ZeRQWzuBAQ&oe=653E6103" className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src="https://scontent.fnqn4-1.fna.fbcdn.net/v/t39.30808-6/391626291_7067940753297285_6954890977725599637_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHJ2qLTodrcoNTlc1TIDsCjf99v5hG6fqx_32_mEbp-rLm-XJ6xAz5PCdh40uC9wIrvSm_l331_zryGks6CNy_e&_nc_ohc=d1H5-ZdsUxkAX_J4b95&_nc_ht=scontent.fnqn4-1.fna&oh=00_AfCbot7RlyQnc6fmUHHfWDnlHiW-y5EQwUu3ZeRQWzuBAQ&oe=653E6103" className="logo react" alt="React logo" />
-      </a>
-      <h1>Cotizador de Pokémon</h1>
+      <Header />
       <button onClick={toggleCotizador}>
         {showCotizador ? 'Ver Historial' : 'Volver al Cotizador'}
       </button>
 
-      {showCotizador && (
-        <div id="pokemon-form">
-          <label htmlFor="pokemon-name">Nombre del Pokémon:</label>
-          <select id="pokemon-name">
-            {pokemonData.pokemon.map((pokemon, index) => (
-              <option key={index} value={index}>{pokemon.name}</option>
-            ))}
-          </select>
+      {showCotizador && <CotizadorForm cotizarPokemon={cotizarPokemon} pokemonData={pokemonData} />}
 
-          <label htmlFor="pokemon-height">Altura (en cm):</label>
-          <input type="number" id="pokemon-height" min="23" max="140" />
-
-          <label htmlFor="pokemon-dna">Tipo de ADN (extrato):</label>
-          <select id="pokemon-dna">
-            {pokemonData.pokemon.map((pokemon, index) => (
-              <option key={index} value={index}>{pokemon.dna}</option>
-            ))}
-          </select>
-
-          <button type="button" onClick={() => cotizarPokemon(pokemonData.pokemon[document.getElementById('pokemon-name').value])}>Calcular</button>
-        </div>
-      )}
-
-      <div id="historial">
-        <h2>Historial de Cotizaciones</h2>
-        <ul id="cotizaciones">
-          {cotizaciones.map((cotizacion, index) => (
-            <li key={index}>{cotizacion}</li>
-          ))}
-        </ul>
-      </div>
+      <HistorialCotizaciones cotizaciones={cotizaciones} />
     </div>
   );
 }
