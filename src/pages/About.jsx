17f94d54a888cec3e43b6b0094ff2e5f032fd2ae
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-30 18:00:47 -0300

    123

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <button class="button"><span>Hover </span></button>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Default.jsx
@@ -0,0 +1,5 @@
+const Default = () =>{
+ return <h1>Default</h1>;
+}
+
+export default Default;
