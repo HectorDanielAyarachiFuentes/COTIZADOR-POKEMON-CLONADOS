8cd6402225254dfc205f496158c89973be123c7b
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-30 19:48:12 -0300

    yh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; // Import Link and useNavigate
 import Header from './Header';
 import CotizadorForm from './CotizadorForm';
 import HistorialCotizaciones from './HistorialCotizaciones';
@@ -10,12 +9,12 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Default from "./pages/Default";
 import Dashboard from "./pages/Dashboard";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   const [showCotizador, setShowCotizador] = useState(true);
   const [cotizaciones, setCotizaciones] = useState([]);
   const [cotizacionInfo, setCotizacionInfo] = useState(null);
-  const navigate = useNavigate(); // Get the navigation function
 
   useEffect(() => {
     const cotizacionesLocalStorage = localStorage.getItem('cotizaciones');
@@ -29,41 +28,30 @@ function App() {
   };
 
   const cotizarPokemon = (pokemon) => {
-    // Your logic to calculate the Pokémon quote goes here.
-    // You can use setCotizaciones to update the list of quotes.
+    // Tu lógica para cotizar el Pokémon aquí.
+    // Puedes usar setCotizaciones para actualizar la lista de cotizaciones.
   };
 
   return (
     <div className="pokemon-app">
       <h1 className="pokemon-title">Pokémon Routes</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="about">About</Link>
-          </li>
-          <li>
-            <Link to="dashboard">Dashboard</Link>
-          </li>
-        </ul>
-      </nav>
+      <Routes>
+  <Route path="/" element={<Layout />}>
+    <Route path="about" element={<About />} />
+    <Route index element={<Home />} /> {/* Utiliza 'index' en lugar de '/' */}
+    <Route path="dashboard" element={<Dashboard />} />
+    <Route path="*" element={<Default />} />
+  </Route>
+</Routes>
 
       <Header />
       <button onClick={toggleCotizador}>
-        {showCotizador ? 'View History' : 'Back to Quoter'}
+        {showCotizador ? 'Ver Historial' : 'Volver al Cotizador'}
       </button>
 
       {showCotizador ? (
         <>
-          <CotizadorForm
-            cotizarPokemon={cotizarPokemon}
-            pokemonData={pokemonData}
-            cotizaciones={cotizaciones}
-            setCotizaciones={setCotizaciones}
-            setCotizacionInfo={setCotizacionInfo}
-          />
+          <CotizadorForm cotizarPokemon={cotizarPokemon} pokemonData={pokemonData} cotizaciones={cotizaciones} setCotizaciones={setCotizaciones} setCotizacionInfo={setCotizacionInfo} />
           {cotizacionInfo && <p>{cotizacionInfo}</p>}
         </>
       ) : (
