996893839f9c0dfacc7b81525f9e0dc44bb2c1c0
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-31 17:49:26 -0300

    456

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,9 +3,19 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   const navStyle = {
     display: "flex",
-    justifyContent: "space-around", // Opcional, puedes ajustar esto según tus necesidades
+    justifyContent: "space-around",
     listStyle: "none",
     padding: 0,
+    backgroundColor: "#FDE349", // Color de fondo amarillo
+    border: "2px solid #FFCB05", // Borde amarillo claro
+    borderRadius: "5px",
+  };
+
+  const linkStyle = {
+    color: "#E3350D", // Color de enlace rojo
+    textDecoration: "none",
+    fontSize: "1.2em",
+    fontWeight: "bold",
   };
 
   return (
@@ -13,13 +23,19 @@ const Layout = () => {
       <nav>
         <ul style={navStyle}>
           <li>
-            <Link to="/">Hogar</Link>
+            <Link to="/" style={linkStyle}>
+              Hogar
+            </Link>
           </li>
           <li>
-            <Link to="/about">Nosotros</Link>
+            <Link to="/about" style={linkStyle}>
+              Nosotros
+            </Link>
           </li>
           <li>
-            <Link to="/dashboard">Dedicatoria</Link>
+            <Link to="/dashboard" style={linkStyle}>
+              Dedicatoria
+            </Link>
           </li>
         </ul>
       </nav>
